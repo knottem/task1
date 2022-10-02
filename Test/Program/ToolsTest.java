483ee105a2be86356764ab0fd9a9483606a1fee2
knottem <59744487+knottem@users.noreply.github.com>
2022-10-02 14:54:17 +0200

    Testing

---
+++ Test/Program/ToolsTest.java
@@ -0,0 +1,25 @@
+package Program;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToolsTest extends TestCase {
+
+    @Test
+    void convertLToCl() {
+    }
+
+    @Test
+    void showMessage() {
+    }
+
+    @Test
+    void optionsMessage() {
+    }
+
+    @Test
+    void questionMessage() {
+    }
+}
---
+++ Test/Program/WindowTest.java
@@ -0,0 +1,17 @@
+package Program;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindowTest extends TestCase {
+
+    @Test
+    void askWindow() {
+    }
+
+    @Test
+    void showWindow() {
+    }
+}
