e743f1cf9c6b8b2032891fb87645d6d5ad829500
knottem <59744487+knottem@users.noreply.github.com>
2022-10-02 14:52:29 +0200

    testa la till TEST

---
+++ Test/Program/ProgramTest.java
@@ -0,0 +1,16 @@
+package Program;
+
+import junit.framework.TestCase;
+
+class ProgramTest extends TestCase {
+
+    /*
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+     */
+}
