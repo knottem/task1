c8672b06817a74f6b6d331afe2af8956b6973708
knottem <59744487+knottem@users.noreply.github.com>
2022-10-02 14:56:38 +0200

    Testing

---
+++ Test/Plants/PlantsTest.java
@@ -0,0 +1,19 @@
+package Plants;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+class PlantsTest extends TestCase {
+
+    @Test
+    public void testGetName() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getTypes() {
+    }
+}
