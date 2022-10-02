44fac72f0b526c9273212cfe2678e4103828a795
knottem <59744487+knottem@users.noreply.github.com>
2022-10-02 15:02:07 +0200

    La till testing men tomma

--- Test/Program/WindowTest.java
+++ Test/Program/WindowTest.java
@@ -3,8 +3,6 @@ package Program;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class WindowTest extends TestCase {
 
     @Test
